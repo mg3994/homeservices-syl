7ba05cf63bd30637656801f5bd4df3cdfd3ec84b
mg3994 <manishgautammg7@yahoo.com>
2024-03-29 01:08:45 +0530

    first commit

--- lib/src/core/config/routes/app_pages.dart
+++ lib/src/core/config/routes/app_pages.dart
@@ -1,10 +1,11 @@
 import 'package:get/get.dart';
 import 'package:homeservices/src/features/signup_screen/view/signup_screen.dart';
 
-import '../../../features/authentication_screen/bindings/authentication_binding.dart';
+// import '../../../features/authentication_screen/bindings/authentication_binding.dart';
 import '../../../features/login_screen/bindings/login_binding.dart';
 import '../../../features/login_screen/view/login_screen.dart';
 import '../../../features/signup_screen/bindings/sign_up_binding.dart';
+import '../../../features/splash_screen/bindings/splash_bindings.dart';
 import '../../../features/splash_screen/view/splash_screen.dart';
 import '../../../features/test_screen/bindings/test_binding.dart';
 import '../../../features/test_screen/view/test_screen.dart';
@@ -18,6 +19,7 @@ abstract class AppPages {
     GetPage(
       name: _Paths.splash,
       page: () => const SplashScreen(),
+      binding: SplashBinding(),
     ),
     GetPage(
       name: _Paths.login,
